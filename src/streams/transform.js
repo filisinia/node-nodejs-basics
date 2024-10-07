3f20109114b66478833da01209a5e1e066488eb9
Mariia Petrenko <filisinia@gmail.com>
2024-10-07 12:02:31 +0300

    feat: write code for the 'transform' function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { stdin, stdout } from "node:process";
+import { pipeline } from "node:stream/promises";
+import { Transform } from "node:stream";
+
 const transform = async () => {
-    // Write your code here 
+  const reverseTransform = new Transform({
+    transform(chunk, _, callback) {
+      const reversedChunk =
+        chunk.toString().split("").reverse().join("") + "\n";
+
+      callback(null, reversedChunk);
+    },
+  });
+
+  await pipeline(stdin, reverseTransform, stdout);
 };
 
 await transform();
